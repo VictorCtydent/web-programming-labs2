36be322beaaa90e8f5eabc8b0c7fd62ec46e946a
Victor Dorogin <dorogin.dbnz@mail.ru>
2022-12-22 16:52:52 +0700

    Вывод даты

---
+++ lab8.js
@@ -0,0 +1,8 @@
+function showDate(params) {
+    let out = document.getElementById('today');
+    let today = new Date();
+    out.innerHTML = today.toLocaleDateString("ru-RU");
+}
+
+
+window.addEventListener('load', showDate);
