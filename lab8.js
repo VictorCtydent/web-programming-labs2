f5eae4ece4dd4bbfa55818d25aee926e8b7f2dea
Victor Dorogin <dorogin.dbnz@mail.ru>
2022-12-22 17:17:23 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -3,6 +3,14 @@ function showDate(params) {
     let today = new Date();
     out.innerHTML = today.toLocaleDateString("ru-RU");
 }
-
+function showDaysCount(parasm) {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date (inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.floor(daysCount); // округляем в меньшую сторону
+    result.innerHTML = 'С даты рождения прошло дней:' + daysCount; 
+}
 
 window.addEventListener('load', showDate);
