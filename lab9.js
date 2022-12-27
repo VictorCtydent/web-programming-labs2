2431b8a46eb91af86bce3a5571e84e34967ad685
Victor Dorogin <dorogin.dbnz@mail.ru>
2022-12-27 10:30:31 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -10,3 +10,23 @@ function student(button) {
     else
     button.innerHTML = 'Дорогин'
 }
+function knopka(button) {
+    if(button.innerHTML == 'Нажми меня') {
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'red';
+    }
+    else if(button.innerHTML == 'Ещё раз нажми') {
+        button.innerHTML = 'Ещё';  
+        button.style.color = 'blue';
+    }
+         
+    else if(button.innerHTML == 'Ещё') {
+        button.innerHTML = 'Начать сначала';   
+        button.style.color = 'pink';
+    }
+        
+    else {
+        button.innerHTML = 'Нажми меня' 
+        button.style.color = 'violet'; 
+    }
+}
